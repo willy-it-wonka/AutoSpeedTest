78d17e82f018c427b3214495c92001dd573fa5ac
Willy <willy.it.wonka@outlook.com>
2025-01-30 21:35:10 +0100

    Open browser in private mode

--- speedtest.py
+++ speedtest.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 
-driver = webdriver.Firefox(service=Service("/snap/bin/geckodriver"))
+options = Options()
+options.add_argument("-private-window")
+
+driver = webdriver.Firefox(service=Service("/snap/bin/geckodriver"), options=options)
 
 driver.get("http://www.speedtest.net")
