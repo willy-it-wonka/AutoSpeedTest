99685a2cc9dcc449e17fc968cca60823dbd96029
Willy <willy.it.wonka@outlook.com>
2025-01-30 22:42:46 +0100

    Refactor speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -9,9 +9,11 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
 GECKODRIVER_PATH = "/snap/bin/geckodriver"
+SPEEDTEST_URL = "http://www.speedtest.net"
 ACCEPT_XPATH = '//*[@id="onetrust-accept-btn-handler"]'
 START_TEST_XPATH = '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]'
 PRIVACY_POLICY_XPATH = '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/a'
+BUTTON_NOT_FOUND_ERROR = "{} was not found while waiting."
 
 
 def run_firefox():
@@ -24,41 +26,39 @@ def run_firefox():
     return browser
 
 
-driver = run_firefox()
+def open_speedtest_page(driver):
+    driver.get(SPEEDTEST_URL)
 
 
-def open_speedtest_page():
-    driver.get("http://www.speedtest.net")
-
-
-def click_if_present(by, xpath, timeout):
+def click_if_present(driver, timeout, by, xpath):
     try:
         button = WebDriverWait(driver, timeout).until(
             ec.element_to_be_clickable((by, xpath))
         )
         button.click()
     except (TimeoutException, NoSuchElementException):
-        print(f"{xpath} was not found while waiting.")
+        print(BUTTON_NOT_FOUND_ERROR.format(xpath))
 
 
-def accept_cookies():
-    click_if_present(By.XPATH, ACCEPT_XPATH, 5)
+def accept_cookies(driver):
+    click_if_present(driver, 5, By.XPATH, ACCEPT_XPATH)
 
 
-def start_speedtest():
-    click_if_present(By.XPATH, START_TEST_XPATH, 1)
+def start_speedtest(driver):
+    click_if_present(driver, 1, By.XPATH, START_TEST_XPATH)
 
 
-def close_privacy_policy():
-    click_if_present(By.XPATH, PRIVACY_POLICY_XPATH, 1)
+def close_privacy_policy(driver):
+    click_if_present(driver, 1, By.XPATH, PRIVACY_POLICY_XPATH)
 
 
-open_speedtest_page()
-
-try:
-    accept_cookies()
-    start_speedtest()
-    close_privacy_policy()
-    time.sleep(90)  # TODO: screenshot
-finally:
-    driver.quit()
+def run_speedtest():
+    driver = run_firefox()
+    try:
+        open_speedtest_page(driver)
+        accept_cookies(driver)
+        start_speedtest(driver)
+        close_privacy_policy(driver)
+        time.sleep(90)  # TODO: screenshot
+    finally:
+        driver.quit()
