469142d3495a85c453b080bb6ccc03ec91d7a4ca
Willy <willy.it.wonka@outlook.com>
2025-01-30 23:52:07 +0100

    Perform speedtest 5 times

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from screenshot import setup_screenshot_folder
-from speedtest import run_speedtest
+import subprocess
+import time
 
-setup_screenshot_folder()
-run_speedtest()
+from screenshot import setup_screenshot_folder
+
+NUM_TESTS = 5
+WAIT_TIME = 10
+RUN_TEST_MESSAGE = "Run speedtest {}/{}..."
+WAITING_MESSAGE = "Waiting {} seconds before the next test...\n"
+FINAL_MESSAGE = "All tests finished."
+
+
+def run_speed_tests():
+    setup_screenshot_folder()
+
+    for i in range(NUM_TESTS):
+        print(RUN_TEST_MESSAGE.format(i + 1, NUM_TESTS))
+        subprocess.run(["python3", "speedtest.py"])
+
+        if i < NUM_TESTS - 1:
+            print(WAITING_MESSAGE.format(WAIT_TIME))
+            time.sleep(WAIT_TIME)
+
+    print(FINAL_MESSAGE)
+
+
+if __name__ == "__main__":
+    run_speed_tests()
