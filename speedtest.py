12dd10228cc340e8c1363e07753cb394328f0848
Willy <willy.it.wonka@outlook.com>
2025-01-30 22:24:48 +0100

    Perform speed test

--- speedtest.py
+++ speedtest.py
@@ -1,11 +1,17 @@
+import time
+
 from selenium import webdriver
 from selenium.common import TimeoutException, NoSuchElementException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 
 GECKODRIVER_PATH = "/snap/bin/geckodriver"
+ACCEPT_XPATH = '//*[@id="onetrust-accept-btn-handler"]'
+START_TEST_XPATH = '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]'
+PRIVACY_POLICY_XPATH = '/html/body/div[3]/div[1]/div[3]/div/div/div/div[2]/div[1]/div/div/div/a'
 
 
 def run_firefox():
@@ -35,4 +41,24 @@ def click_if_present(by, xpath, timeout):
         print(f"{xpath} was not found while waiting.")
 
 
+def accept_cookies():
+    click_if_present(By.XPATH, ACCEPT_XPATH, 5)
+
+
+def start_speedtest():
+    click_if_present(By.XPATH, START_TEST_XPATH, 1)
+
+
+def close_privacy_policy():
+    click_if_present(By.XPATH, PRIVACY_POLICY_XPATH, 1)
+
+
 open_speedtest_page()
+
+try:
+    accept_cookies()
+    start_speedtest()
+    close_privacy_policy()
+    time.sleep(90)  # TODO: screenshot
+finally:
+    driver.quit()
