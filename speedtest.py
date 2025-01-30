fd2b1499477fd4d0795fbbd1ac4f441d02222b1f
Willy <willy.it.wonka@outlook.com>
2025-01-30 22:07:25 +0100

    Add a reusable method for clicking buttons

--- speedtest.py
+++ speedtest.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.common import TimeoutException, NoSuchElementException
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
 
 GECKODRIVER_PATH = "/snap/bin/geckodriver"
 
@@ -20,6 +23,16 @@ driver = run_firefox()
 
 def open_speedtest_page():
     driver.get("http://www.speedtest.net")
+
+
+def click_if_present(by, xpath, timeout):
+    try:
+        button = WebDriverWait(driver, timeout).until(
+            ec.element_to_be_clickable((by, xpath))
+        )
+        button.click()
+    except (TimeoutException, NoSuchElementException):
+        print(f"{xpath} was not found while waiting.")
 
 
 open_speedtest_page()
