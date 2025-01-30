c9d43f1319607dccbf842bf77509ce6f17890750
Willy <willy.it.wonka@outlook.com>
2025-01-30 21:58:50 +0100

    Refactor speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -2,10 +2,24 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
 
-options = Options()
-options.add_argument("-private-window")
+GECKODRIVER_PATH = "/snap/bin/geckodriver"
 
-driver = webdriver.Firefox(service=Service("/snap/bin/geckodriver"), options=options)
-driver.maximize_window()
 
-driver.get("http://www.speedtest.net")
+def run_firefox():
+    options = Options()
+    options.add_argument("-private-window")
+
+    browser = webdriver.Firefox(service=Service(GECKODRIVER_PATH), options=options)
+    browser.maximize_window()
+
+    return browser
+
+
+driver = run_firefox()
+
+
+def open_speedtest_page():
+    driver.get("http://www.speedtest.net")
+
+
+open_speedtest_page()
